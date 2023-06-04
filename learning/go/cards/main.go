4f19dc71edcfcf06ffc93f8bf1c7c5d88a6d7f5e
datvt28 <nhkkin76@gmail.com>
2023-06-04 14:19:59 +0700

    sssss

--- learning/go/cards/main.go
+++ learning/go/cards/main.go
@@ -1,9 +1,7 @@
 package main
-
 func main() {
-	cards := newDeck()
-
-	hand, remainingCards := deal(cards, 5)
-	hand.print()
-	remainingCards.print()
+	// cards := newDeck()
+	// cards.saveToFile("my_cards")
+	cards := newDeckFromFile("my_cards")
+	cards.print()
 }
