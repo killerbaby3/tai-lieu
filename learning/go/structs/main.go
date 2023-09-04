41f7ecba7086b2e5c0c31b53c20daef32abe7ca5
datvt28 <nhkkin76@gmail.com>
2023-09-04 22:08:45 +0700

    ssss

--- learning/go/structs/main.go
+++ learning/go/structs/main.go
@@ -1,10 +1,36 @@
 package main
 
+import "fmt"
+
+type contactInfo struct {
+	email string
+	zipCode int
+}
+
 type person struct {
-	firstname string
-	lastname  string
+	firstName string
+	lastName  string
+	contactInfo
 }
 
 func main() {
+	jim := person{
+		firstName:"Thanh",
+		lastName:"Dat",
+		contactInfo: contactInfo{
+			email:"nhkkin76@gmail.com",
+			zipCode:7000,
+		},
+	}
+	jimPointer := &jim
+	jimPointer.updateName("Jimmy")
+	jim.print()
+}
 
+func (pointerToPerson *person) updateName(newFirstName string)  {
+	(*pointerToPerson).firstName = newFirstName
+}
+
+func (p person) print()  {
+	fmt.Printf("%+v", p)
 }
